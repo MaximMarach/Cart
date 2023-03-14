2ba9f531bc000b7839613867c4ebaba6e4f67a6f
max <maracmaksim53@gmail.com>
2023-03-14 16:11:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function handler_forward(){
+  console.log('forward')
+  return true
+}
+
+function handler_left(){
+  console.log('left')
+  return true
+}
+
+function handler_right() {
+  console.log('right')
+  return true
+}
+
+// forward_button.addEventListener('click', f_b)
